1ea3610d7f473703bc4da217a2d781b0f79353b3
Trang Nguyen <trangnguyen.281198@gmail.com>
2020-04-23 13:59:56 +0300

    Update gh-pages

---
+++ precache-manifest.914d16dc4335127a4d6d60b7f9291b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70055d60142b17f790c49a191ca59f3e",
+    "url": "/3d-editor/index.html"
+  },
+  {
+    "revision": "88eb121a394f71ab2f57",
+    "url": "/3d-editor/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "48a9fca51c7e07a69c63",
+    "url": "/3d-editor/static/css/main.5b59cfa6.chunk.css"
+  },
+  {
+    "revision": "88eb121a394f71ab2f57",
+    "url": "/3d-editor/static/js/2.0cf879f8.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/3d-editor/static/js/2.0cf879f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a9fca51c7e07a69c63",
+    "url": "/3d-editor/static/js/main.76d12514.chunk.js"
+  },
+  {
+    "revision": "68a666b7bd9c2a67a62f",
+    "url": "/3d-editor/static/js/runtime-main.2e6e19a6.js"
+  }
+]);
